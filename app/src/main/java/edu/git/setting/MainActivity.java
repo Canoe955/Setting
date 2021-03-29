d9bf6f463e4815ad39945ed32f77330593388feb
canoe <canoe955yan@gmail.com>
2021-03-29 11:16:05 +0800

    QQsetting page 1.1beat

---
+++ app/src/main/java/edu/git/setting/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.git.setting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
